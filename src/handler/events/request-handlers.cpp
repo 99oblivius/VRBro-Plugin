f09d64bf0feb4817e2c33a6511a3c9c52640bd34
99oblivius <99dark.oblivion99@gmail.com>
2024-10-13 01:44:01 +0200

    working on linux and macos building test

--- src/handler/events/request-handlers.cpp
+++ src/handler/events/request-handlers.cpp
@@ -1,18 +1,22 @@
+#define UNUSED(x) (void)(x)
 #include "request-handlers.hpp"
 
 void RequestHandler::is_replay_buffer(std::string& msg, handle_error& ec) {
+    UNUSED(ec);
     if (obs_frontend_replay_buffer_active()) {
         msg = "recording buffer";
     }
 }
 
 void RequestHandler::is_recording(std::string& msg, handle_error& ec) {
+    UNUSED(ec);
     if (obs_frontend_recording_active()) {
         msg = "recording";
     }
 }
 
 void RequestHandler::is_streaming(std::string& msg, handle_error& ec) {
+    UNUSED(ec);
     if (obs_frontend_streaming_active()) {
         msg = "streaming";
     }
