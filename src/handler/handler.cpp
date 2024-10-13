962a235f886fc5d0e7ef4fac8bcda4d4ce033c48
99oblivius <99dark.oblivion99@gmail.com>
2024-10-14 00:43:50 +0200

    adjusted event and request packet header protocol

--- src/handler/handler.cpp
+++ src/handler/handler.cpp
@@ -20,13 +20,13 @@ void CommandHandler::Handler(std::string_view& packet, std::vector<uint8_t>& res
         action = packet[0] & 0x3F;
         if ((uint8_t)packet[0] & 0x80) {
             EventHandler(packet, action, msg, ec);
+            header |= 1 << 7;
         } else {
             RequestHandler(packet, action, msg, ec);
-            header |= (msg.empty() ? 0 : 1) << 7;
         }
     }
 
-    header |= ((bool)ec ? 0 : 1) << 6;
+    header |= ((msg.empty() ? 0 : 1) | (bool)ec ? 0 : 1) << 6;
     header |= static_cast<uint8_t>(action) & 0x3F;
     response.push_back(header);
     
